fb62040a9b36a89736a96a8b1aaf82011accfc55
wlanboy <me@wlanboy.com>
2023-03-26 12:42:30 +0200

    unused imports

--- src/test/java/com/wlanboy/reactive/tick/TickRouterTest.java
+++ src/test/java/com/wlanboy/reactive/tick/TickRouterTest.java
@@ -1,11 +1,9 @@
 package com.wlanboy.reactive.tick;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
